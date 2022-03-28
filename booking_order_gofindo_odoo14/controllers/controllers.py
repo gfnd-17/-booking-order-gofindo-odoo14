c3119107fe50a95b0ee49a9e0b23e3046b18a1d4
Gofindo <91790461+gofindo17@users.noreply.github.com>
2022-03-28 10:40:06 +0700

    Update controllers.py

--- booking_order_gofindo_odoo14/controllers/controllers.py
+++ booking_order_gofindo_odoo14/controllers/controllers.py
@@ -2,20 +2,20 @@
 # from odoo import http
 
 
-# class BookingOrderZahra14(http.Controller):
-#     @http.route('/booking_order_zahra_14/booking_order_zahra_14/', auth='public')
+# class BookingOrderGofindoOdoo14(http.Controller):
+#     @http.route('/booking-order-gofindo-odoo14/booking-order-gofindo-odoo14/', auth='public')
 #     def index(self, **kw):
 #         return "Hello, world"
 
-#     @http.route('/booking_order_zahra_14/booking_order_zahra_14/objects/', auth='public')
+#     @http.route('/booking-order-gofindo-odoo14/booking-order-gofindo-odoo14/objects/', auth='public')
 #     def list(self, **kw):
-#         return http.request.render('booking_order_zahra_14.listing', {
-#             'root': '/booking_order_zahra_14/booking_order_zahra_14',
-#             'objects': http.request.env['booking_order_zahra_14.booking_order_zahra_14'].search([]),
+#         return http.request.render('booking-order-gofindo-odoo14.listing', {
+#             'root': '/booking-order-gofindo-odoo14/booking-order-gofindo-odoo14',
+#             'objects': http.request.env['booking-order-gofindo-odoo14.booking-order-gofindo-odoo14'].search([]),
 #         })
 
-#     @http.route('/booking_order_zahra_14/booking_order_zahra_14/objects/<model("booking_order_zahra_14.booking_order_zahra_14"):obj>/', auth='public')
+#     @http.route('/booking-order-gofindo-odoo14/booking-order-gofindo-odoo14/objects/<model("booking-order-gofindo-odoo14.booking-order-gofindo-odoo14"):obj>/', auth='public')
 #     def object(self, obj, **kw):
-#         return http.request.render('booking_order_zahra_14.object', {
+#         return http.request.render('booking-order-gofindo-odoo14.object', {
 #             'object': obj
 #         })
